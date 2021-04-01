3ca6cf52f9bf45062c00fbb956bb2ec17202e5d2
Makarov ilya <74796903+Bakl9@users.noreply.github.com>
2021-04-01 12:15:18 +0300

    Create minin,learn.js

---
+++ minin,learn.js
@@ -0,0 +1,8 @@
+var cars = ['audi', 'bmv', 'mersedes']
+
+ cars.push('volvo') // добавления 
+ cars.shift() // Удаляет и возращаяет последний элемент
+ cars.unshift('bentley') //Добавляет элемент в первую позицыю 
+
+ console.log(cars.indexOf('bmv')) //проверяет по какому счету лежитэлемент 
+ console.log(cars)
