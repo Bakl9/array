7aa5c2c9f80d3275a220fd320083ec09f7c2813f
Makarov ilya <74796903+Bakl9@users.noreply.github.com>
2021-04-06 11:49:51 +0300

    Update minin,learn.js

--- minin,learn.js
+++ minin,learn.js
@@ -1,1 +1,16 @@
 
+
+const lolPeople = people.map(person => `${person.name} (${person.age})`);
+console.log(lolPeople);
+
+
+const adults = people.filter(person => person.age <= 30);
+console.log(adults);
+
+const amourt = people.reduce((total, person) => total + person.budget, 0);
+console.log(amourt);
+
+people.forEach(person => console.log(person));
+
+const igor = people.find(person => person.name === 'Игорь');
+console.log(igor)
