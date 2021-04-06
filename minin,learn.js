97995aefbb27260cd1502dc4190b6aff953ad2a2
Makarov ilya <74796903+Bakl9@users.noreply.github.com>
2021-04-06 12:12:49 +0300

    Update minin,learn.js

--- minin,learn.js
+++ minin,learn.js
@@ -8,17 +8,20 @@ const people = [
     { name: 'Виктория', age: 18, budget: 2300},
 ];
 
-const lolPeople = people.map(person => `${person.name} (${person.age})`);
-console.log(lolPeople);
-
-
-const adults = people.filter(person => person.age <= 30);
-console.log(adults);
-
-const amourt = people.reduce((total, person) => total + person.budget, 0);
-console.log(amourt);
-
 people.forEach(person => console.log(person));
 
-const igor = people.find(person => person.name === 'Игорь');
-console.log(igor)
+const indexIgor = people.find(person => person.name === 'Игорь');
+console.log(indexIgor);
+
+const newPeople = people.map(person => `${person.name} (${person.budget})`);
+console.log(newPeople);
+
+const adults = people.reduce((total, person) => total + person.age, 0);
+console.log(adults);
+
+const many = people.filter(person => person.budget >= 3000);
+console.log(many);
+
+const indexVladelen = people.findIndex( person => person.name === 'Владилен');
+console.log(indexVladelen)
+
