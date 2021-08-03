57f5751395cd72b2ff382ab7471d65f51cf4301f
Makarov ilya <74796903+Bakl9@users.noreply.github.com>
2021-08-03 20:29:33 +0300

    Update minin,learn.js

--- minin,learn.js
+++ minin,learn.js
@@ -4,7 +4,7 @@ const people = [
     { name: 'Елена', age: 26, budget: 3400},
     { name: 'Игорь', age: 40, budget: 5000},  
     { name: 'Михаил', age: 55, budget: 1800},
-    { name: 'Василиса', age: 24, budget: 1500},
+    { name: 'Василиса', age: 24, budget: 1500}, 
     { name: 'Виктория', age: 18, budget: 2300},
 ];
 
